a37c454e51eca68d56e809afd89f2c1e4fe86ef0
Veera1789 <veeramaninatarajanmca@gmail.com>
2017-02-16 23:07:22 +0530

    Average Weightage probabilities

--- AV_LoanPrediction3.R
+++ AV_LoanPrediction3.R
@@ -1,6 +1,5 @@
 # Loan Prediction 3 Hackathon - Analytics vidhya 
 # AV Article Code Version
-
 library(caret)
 library(mice)
 library(gmodels)
@@ -15,9 +14,8 @@ train<-read.csv("train.csv",header = T)
 str(train)
 sum(is.na(train))
 ################
-#Imputing missing values using median
+#Imputing missing values using median for training data
 preProcValues <- preProcess(train, method = c("medianImpute","center","scale"))
-library('RANN')
 data_processed <- predict(preProcValues, train)
 sum(is.na(data_processed))
 
@@ -51,8 +49,6 @@ testSet$pred_rf<-predict(object = model_rf,testSet[,predictors])
 
 #Checking the accuracy of the random forest model
 confusionMatrix(testSet$Loan_Status,testSet$pred_rf)
-
-
 ###################################################################################################
 #Training the knn model
 model_knn<-train(trainSet[,predictors],trainSet[,outcomeName],method='knn',trControl=fitControl,tuneLength=3)
@@ -64,39 +60,33 @@ testSet$pred_knn<-predict(object = model_knn,testSet[,predictors])
 confusionMatrix(testSet$Loan_Status,testSet$pred_knn)
 
 ##################################################################################################
-
 #Training the Logistic regression model
 model_lr<-train(trainSet[,predictors],trainSet[,outcomeName],method='glm',trControl=fitControl,tuneLength=3)
 
-#Predicting using knn model
+#Predicting using Log reg model
 testSet$pred_lr<-predict(object = model_lr,testSet[,predictors])
 
-#Checking the accuracy of the random forest model
+#Checking the accuracy of the lr model
 confusionMatrix(testSet$Loan_Status,testSet$pred_lr)
 #################################################################################################
-# Prediction
+# imputting missing values
 test<-read.csv("test.csv",header = T)
 preProcValues_test <- preProcess(test, method = c("medianImpute","center","scale"))
 data_processed_test <- predict(preProcValues_test, test)
 sum(is.na(data_processed_test))
 
-# Random Forest
-data_processed_test$pred_rf<-predict(object = model_rf,data_processed_test[,predictors])
-Submission<-data.frame(Loan_ID=data_processed_test$Loan_ID,Loan_Status=data_processed_test$pred_rf,stringsAsFactors=FALSE)
+# Predicting Probability 
+data_processed_test$pred_rf_prob<-predict(object = model_rf,data_processed_test[,predictors],type='prob')
+data_processed_test$pred_knn_prob<-predict(object = model_knn,data_processed_test[,predictors],type='prob')
+data_processed_test$pred_lr_prob<-predict(object = model_lr,data_processed_test[,predictors],type='prob')
+
+#Taking average of predictions
+data_processed_test$pred_avg<-(data_processed_test$pred_rf_prob$Y+data_processed_test$pred_knn_prob$Y+data_processed_test$pred_lr_prob$Y)/3
+
+#Splitting into binary classes at 0.5
+data_processed_test$pred_avg<-as.factor(ifelse(data_processed_test$pred_avg>0.5,'Y','N'))
+
+#Writing Files
+Submission<-data.frame(Loan_ID=data_processed_test$Loan_ID,Loan_Status=data_processed_test$pred_avg,stringsAsFactors=FALSE)
 summary(Submission)
 write.csv(Submission,file="Submission.csv",row.names = F)
-
-# KNN Prediction
-data_processed_test$pred_knn<-predict(object = model_knn,data_processed_test[,predictors])
-Submission<-data.frame(Loan_ID=data_processed_test$Loan_ID,Loan_Status=data_processed_test$pred_knn,stringsAsFactors=FALSE)
-summary(Submission)
-write.csv(Submission,file="Submission.csv",row.names = F)
-
-
-# Logistic  Prediction
-data_processed_test$pred_lr<-predict(object = model_lr,data_processed_test[,predictors])
-Submission<-data.frame(Loan_ID=data_processed_test$Loan_ID,Loan_Status=data_processed_test$pred_lr,stringsAsFactors=FALSE)
-summary(Submission)
-write.csv(Submission,file="Submission.csv",row.names = F)
-
-
